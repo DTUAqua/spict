79bedc39553226be70207ca0a4aae9ee82b54ea6
Martin W Pedersen <wpsgodd@gmail.com>
2014-11-17 10:01:00 +0100

    Added description file

---
+++ spict/R/Description.R
@@ -0,0 +1,12 @@
+#' @name spict
+#' @aliases spict
+#' @docType package
+#' @title Fits a continuous-time surplus production model to data
+#' @author Martin W. Pedersen \email{map@@aqua.dtu.dk}
+#' @references
+#' \url{https://github.com/martinwpedersen/spict/}
+#' @keywords production model, fisheries, assessment
+#' @seealso \code{\link{test.spict}}
+#' @examples
+#' rep <- test.spict()
+NULL
