6aa1d60723487f91e2ac046d7e289b5f0e76421c
martinwpedersen <wpsgodd@gmail.com>
2015-10-09 10:18:34 +0200

    Added license information

--- spict/R/osar.R
+++ spict/R/osar.R
@@ -1,3 +1,20 @@
+# Stochastic surplus Production model in Continuous-Time (SPiCT)
+#    Copyright (C) 2015  Martin Waever Pedersen, mawp@dtu.dk or wpsgodd@gmail.com
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
 #' @name calc.osa.resid
 #' @title Calculate one-step-ahead residuals.
 #' @details In TMB one-step-ahead residuals are calculated by sequentially including one data point at a time while keeping the model parameters fixed at their ML estimates. The calculated residuals are tested for independence in lag 1 using the Ljung-Box test (see Box.test).
