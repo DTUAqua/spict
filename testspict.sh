3761a15f32f9e98acc3ad5d63d2a2ccece984d17
Alexandros Kokkalis <alko@aqua.dtu.dk>
2017-11-22 23:04:55 +0100

    Added test using the vignette, updated vignette pdf

--- testspict.sh
+++ testspict.sh
@@ -1,3 +1,76 @@
 #!/bin/bash
-./package.sh
-echo "library(spict); example(spict)" | R --slave
+SECONDS=0
+tmpdir=$(mktemp -d)
+RED='\033[31m'
+
+ref="master"
+vignfn="spict/vignettes/vignette.Rmd"
+
+while getopts ":r:v:" opt; do
+  case $opt in
+    r)
+      ref=$OPTARG
+  ;;
+    v)
+      vignfn=$OPTARG
+  ;;
+  :)
+      echo "Option -$OPTARG requires an argument." >&2
+      exit 1
+      ;;
+  esac
+done
+
+echo "Working on temporary directory: " $tmpdir
+
+printmsg () {
+  CYAN='\033[96m'
+  NC='\033[0m' # No Color
+  printf "${CYAN}*************************************************\n"
+  printf "$1\n"
+  printf "*************************************************${NC}\n"
+}
+
+
+printmsg "Installing spict from github, reference:  $ref"
+echo "withr::with_libpaths(new = '$tmpdir', {devtools::install_cran('ellipse', quiet = TRUE, repo = 'http://cran.rstudio.com')})" | R --slave
+echo "withr::with_libpaths(new = '$tmpdir', {devtools::install_github('mawp/spict/spict', ref = '$ref', quiet = TRUE)})" | R --slave
+
+
+printmsg "Run vignette examples"
+
+../runexamples.R -r $vignfn -o $tmpdir -l $tmpdir -f old.md
+
+
+printmsg "Install local spict version"
+
+echo "withr::with_libpaths(new = '$tmpdir', {devtools::install_local('spict', quiet = TRUE)})" | R --slave
+
+printmsg "Run vignette examples"
+
+../runexamples.R -r $vignfn -o $tmpdir -l $tmpdir -f new.md
+
+
+
+printmsg "All examples are done using the two verions of R\nTime ellapsed" $SECONDS "seconds"
+
+
+printmsg "Compare examples"
+
+
+if [ ! -e $tmpdir/new.md ] || [ ! -e $tmpdir/old.md ]; then
+  printf "${RED}The example files are not found\n"
+  exit 124
+fi
+
+newhash=$(md5sum $tmpdir/new.md | cut -d " " -f1)
+oldhash=$(md5sum $tmpdir/old.md | cut -d " " -f1)
+
+if [ $newhash == $oldhash ] ; then 
+  echo "The two versions of spict are producing the same results."
+else 
+  printf "${RED}There are differences in the two versions of spict."
+  exit 1
+fi
+
+exit
