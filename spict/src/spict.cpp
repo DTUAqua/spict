1b2dde9f949e731b0d479825feeb213840e94ba9
kaskr <kaskr@dtu.dk>
2020-10-23 16:35:05 +0200

    Kasper (#93)
    * un-initialized variable
    
    * Add TMB_LIB_INIT

--- spict/src/spict.cpp
+++ spict/src/spict.cpp
@@ -17,6 +17,7 @@
 */
 
 // 14.10.2014
+#define TMB_LIB_INIT R_init_spict
 #include <TMB.hpp>
 
 /* Predict biomass */
@@ -651,7 +652,7 @@ Type objective_function<Type>::operator() ()
 
     // Seasonal component
     if(dbg>0){ std::cout << "-- seasontype: " << seasontype << std::endl; }
-    for(int i=1; i<ns; i++) logS(i) = 0.0; // Initialise
+    for(int i=0; i<ns; i++) logS(i) = 0.0; // Initialise
     if(seasontype == 1 || seasontype == 3 ){
       // Spline
       int ind2, ind3;
