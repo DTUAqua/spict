9939375c83457adca90ef8ecf04c8369f8cff84e
Alexandros Kokkalis <alko@aqua.dtu.dk>
2021-01-28 12:21:34 +0100

    Testmore

--- testmore/funcs.R
+++ testmore/funcs.R
@@ -1,8 +1,9 @@
 ## General functions for testmore
+if (! "cnsl" %in% ls()) cnsl <- FALSE
 
 ## Test functions
-out <- function(..., sep = "", append = TRUE, cnsl = FALSE){
-    if(cnsl){
+out <- function(..., sep = "", append = TRUE){
+    if (cnsl) {
         cat(..., "\n", sep = sep)
     }else{
         cat(..., "\n", file = "res.out", append = append, sep = sep)
@@ -13,12 +14,12 @@ get_nchar <- function(...){
     nchar(paste(as.character(unlist(list(...))), collapse = " "))
 }
 
-header <- function(..., append = TRUE, cnsl = FALSE){
-    out("\n", ..., "\n", rep("=", get_nchar(...)), "\n", append = append, cnsl=cnsl)
+header <- function(..., append = TRUE){
+    out("\n", ..., "\n", rep("=", get_nchar(...)), "\n", append = append)
 }
 
-test_this <- function(title="", expression, cnsl = FALSE) {
-  out(title, cnsl=cnsl)
-  tryCatch(out(capture.output(eval(expression)), sep = "\n", cnsl=cnsl),
-           error = function(e) out("Error:", e$message, cnsl=cnsl))
+test_this <- function(title="", expression) {
+  out(title)
+  tryCatch(out(capture.output(eval(expression)), sep = "\n"),
+           error = function(e) out("Error:", e$message))
 }
