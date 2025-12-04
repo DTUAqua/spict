f1b287f0f82a1b1cb710e37426c6c42b3798ad85
tokami <t.k.mildenberger@gmail.com>
2025-12-04 23:34:25 +0100

    updated vignettes

--- spict/vignettes/spict_guidelines.R
+++
@@ -1,4 +0,0 @@
-## ---- echo = FALSE, warning = FALSE, message = FALSE--------------------------
-knitr::opts_chunk$set(collapse = TRUE, comment = "  ", fig.align = 'center', cache=FALSE)
-library(knitr)
-
